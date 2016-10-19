76e0e78698f19c6f4c5ad8342f1c8e4ca22a632e
Nicola Pedretti <np.pedretti@gmail.com>
2016-10-19 08:51:54 -0500

    syntax refactoring

--- app/frontend/react_components/meal.jsx
+++ app/frontend/react_components/meal.jsx
@@ -1,128 +1,113 @@
 var React = require('react')
-var _     = require('underscore')
-var $     = require('jquery')
+var _ = require('underscore')
+var $ = require('jquery')
 
 var Meal = React.createClass({
+  getInitialState: function () {
+    return {
+      orders: this.props.orders
+    }
+  },
 
-	getInitialState: function () {
-		return {
-			orders: this.props.orders
-		}
-	},
+  propTypes: {
+    _id: React.PropTypes.string.isRequired,
+    name: React.PropTypes.string.isRequired,
+    prices: React.PropTypes.array.isRequired,
+    people: React.PropTypes.array.isRequired,
+    orders: React.PropTypes.number.isRequired,
+    image: React.PropTypes.string.isRequired
+  },
 
-	propTypes: {
-		_id: React.PropTypes.string.isRequired,
-		name: React.PropTypes.string.isRequired,
-		prices: React.PropTypes.array.isRequired,
-		people: React.PropTypes.array.isRequired,
-		orders: React.PropTypes.number.isRequired,
-		image: React.PropTypes.string.isRequired
-	},
+  getDefaultPrice: function () {
+    return this.props.prices[0]
+  },
 
-	getDefaultPrice: function () {
-		return this.props.prices[0]
-	},
+  getCurrentPrice: function () {
+    var people = this.props.people
+    for (var i = 0; i < _.size(people); i++) {
+      if (people[i] > this.state.orders) {
+        return this.props.prices[--i]
+      }
+    }
+    return _.min(this.props.prices)
+  },
 
-	getCurrentPrice: function () {
-		var people = this.props.people
-		for (var i = 0; i < _.size(people); i++) {
-			if (people[i] > this.state.orders) {
-				return this.props.prices[--i]
-			}
-		}
-		return _.min(this.props.prices)
-	},
+  getNextPricePeople: function () {
+    var people = this.props.people
+    var prices = this.props.prices
+    for (var i = 0; i < _.size(people); i++) {
+      if (people[i] > this.state.orders) {
+        return {people: people[i],price: prices[i]}
+      }
+    }
+    return this.getBestPricePeople()
+  },
 
-	getNextPricePeople: function () {
-		var people = this.props.people
-		var prices = this.props.prices
-		for (var i = 0; i < _.size(people); i++) {
-			if (people[i] > this.state.orders) {
-				return {people: people[i],price: prices[i]}
-			}
-		}
-		return this.getBestPricePeople()
-	},
+  getBestPricePeople: function () {
+    var people = this.props.people
+    var prices = this.props.prices
+    return {people: _.max(people), price: _.min(prices)}
+  },
 
-	getBestPricePeople: function () {
-		var people = this.props.people
-		var prices = this.props.prices
-		return {people: _.max(people), price: _.min(prices)}
-	},
+  addOrder: function () {
+    var query = {meal: this.props._id}
+    $.ajax({
+      method: 'POST',
+      url: '/order',
+      data: query,
+      error: (data) => {
+        this.setState({orders: this.state.orders - 1})
+        alert('something went wrong, we could not process your order')
+      }
+    })
+    this.setState({orders: this.state.orders + 1})
+  },
 
-	addOrder: function () {
-		var query = {meal: this.props._id}
-		$.ajax({
-			method: "POST",
-			url: "/order",
-			data: query,
-			error: (data) => {
-				this.setState({orders: this.state.orders-1})
-				alert('something went wrong, we could not process your order')
-			}
-		})
-		this.setState({orders: this.state.orders+1})
-	},
-
-	render: function() {
-		var defaultPrice = this.getDefaultPrice()
-		var currentPrice = this.getCurrentPrice()
-		var bestPricePeople = this.getBestPricePeople()
-		var nextPricePeople = this.getNextPricePeople()
-		return (
-			<div className="col-xs-12 col-sm-6 col-md-4 meal">
-				<div className="thumbnail">
-		      		<img src={"images/meals/"+this.props.image} alt="Meal Picture" />
-		      		<div className="caption clearfix">
-		        		<h3>
-		        			{this.props.name}
-		        		</h3>
-		        		<h4>
-		        			Price:
-		        			{defaultPrice != currentPrice
-								&&
-									<span className='lateral-space'>
-				        				<strike>
-				        					{'$'+defaultPrice}
-				        				</strike>
-			        				</span>
-							}
-							<span className='label-success current-price pull-right'>
-        						{'$'+currentPrice}
-        					</span>
-		        		</h4>
-		        		<hr/>
-		        		{currentPrice == bestPricePeople.price
-		        			?
-			        			<p>
-			        				You are getting our best deal!
-			        				<br/>
-			        				<span className='badge'>
-			        					{this.state.orders}
-			        				</span> people bought it
-			        			</p>
-			        			
-			        		:
-			        			<p>
-			        				If {nextPricePeople.people - this.state.orders} more people buy it, 
-			        				you will pay ${nextPricePeople.price}
-			        				<br/>
-			        				Best deal: {bestPricePeople.people} people, {'$'+bestPricePeople.price}
-			        				<br/>
-			        				<span className='badge'>
-			        					{this.state.orders}
-			        				</span> people bought it
-			        			</p>
-		        		}
-		        		<hr/>
-		        		<p className='pull-right'>
-		        			<a className="btn btn-primary btn-outline" role="button" onClick={this.addOrder}>Order</a>
-		        		</p>
-		     	 	</div>
-		    	</div>
-		  	</div>
-		)
-	}
+  render: function () {
+    var defaultPrice = this.getDefaultPrice()
+    var currentPrice = this.getCurrentPrice()
+    var bestPricePeople = this.getBestPricePeople()
+    var nextPricePeople = this.getNextPricePeople()
+    return (
+      <div className='col-md-6 col-lg-4 meal'>
+        <div className='thumbnail'>
+          <img src={'images/meals/' + this.props.image} alt='Meal Picture' />
+          <div className='caption clearfix'>
+            <h3 className='food-name'>{this.props.name}</h3>
+            <h4>Price {defaultPrice != currentPrice
+                       &&
+                       <span className='lateral-space'><strike>{'$' + defaultPrice}</strike></span>} <span className='label-success current-price pull-right'>{'$' + currentPrice}</span></h4>
+            <hr/>
+            {currentPrice == bestPricePeople.price
+               ?
+               <p>
+                 You are getting our best deal!
+                 <br/>
+                 <span className='badge'>{this.state.orders}</span> people bought it
+               </p>
+             
+               :
+               <p>
+                 If
+                 {" "}
+                 {nextPricePeople.people - this.state.orders} more people buy it, you will pay $
+                 {nextPricePeople.price}
+                 <br/> Best deal:
+                 {" "}
+                 {bestPricePeople.people} people,
+                 {" "}
+                 {'$' + bestPricePeople.price}
+                 <br/>
+                 <span className='badge'>{this.state.orders}</span> people bought it
+               </p>}
+            <p className='pull-right'>
+              <a className='btn btn-primary btn-outline' role='button' onClick={this.addOrder}>Order</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
 })
 
--- app/frontend/react_components/meals_box.jsx
+++ app/frontend/react_components/meals_box.jsx
@@ -1,58 +1,52 @@
-var React    = require('react')
+var React = require('react')
 var ReactDom = require('react-dom')
-var $        = require('jquery')
-var Meal    = require('./meal.jsx')
-var _        = require('underscore')
+var $ = require('jquery')
+var Meal = require('./meal.jsx')
+var _ = require('underscore')
 
 var MealsBox = React.createClass({
+  getInitialState: function () {
+    return {
+      restaurants: []
+    }
+  },
 
-	getInitialState: function () {
-		return {
-			restaurants: []
-		}
-	},
+  componentWillMount: function () {
+    $.ajax({
+      method: 'GET',
+      url: '/meals',
+      success: (data) => {
+        this.setState({restaurants: data})
+      }
+    })
+  },
 
-	componentWillMount: function () {
-		$.ajax({
-			method: "GET",
-			url: "/meals",
-			success: (data) => {
-				this.setState({restaurants:data})
-			}
-		})
-	},
-
-	render: function() {
-		return (
-			<div>
-				{_.map(this.state.restaurants,(restaurant) => {
-						return (
-							<div className= "row" key ={restaurant._id}>
-								<div className='page-header'>
-									<h3>
-										{restaurant.name}
-									</h3>
-								</div>
-								{_.map(restaurant.meals, (meal) => {
-									return <Meal
-										key = {meal._id}
-										_id = {meal._id}
-										name = {meal.name}
-										prices = {meal.prices}
-										people = {meal.people}
-										orders = {_.size(meal.orders)}
-										image = {meal.image}
-									/>
-								})}
-							</div>
-						)	
-
-				})}
-			</div>
-		)
-	}
+  render: function () {
+    return (
+      <div>
+        {_.map(this.state.restaurants, (restaurant) => {
+           return (
+             <div className='row' key={restaurant._id}>
+               <div className='page-header'>
+                 <h3>{restaurant.name}</h3>
+               </div>
+               {_.map(restaurant.meals, (meal) => {
+                  return <Meal
+                           key={meal._id}
+                           _id={meal._id}
+                           name={meal.name}
+                           prices={meal.prices}
+                           people={meal.people}
+                           orders={_.size(meal.orders)}
+                           image={meal.image} />
+                })}
+             </div>
+           )
+         })}
+      </div>
+    )
+  }
 
 })
 
-ReactDom.render(<MealsBox/>,document.getElementById('meals-box'))
-
+ReactDom.render(<MealsBox/>, document.getElementById('meals-box'))
