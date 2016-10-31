a3cde620e5f8ea5fbd2fce1a2a04c508b379b7c3
Nicola Pedretti <np.pedretti@gmail.com>
2016-10-31 11:37:42 -0500

    adding confirmation dialog on buy

--- app/frontend/views/meal/actions.jsx
+++ app/frontend/views/meal/actions.jsx
@@ -1,4 +1,5 @@
 var React = require('react')
+var Modal = require('react-bootstrap').Modal
 
 var Actions = React.createClass({
 
@@ -13,21 +14,57 @@ var Actions = React.createClass({
 		}
 	},
 
+	getInitialState: function () {
+		return {
+			open: false
+		}
+	},
+
+	openDialog: function () {
+		this.setState({open: true})
+	},
+
+	closeDialog: function () {
+		this.setState({open: false})
+	},
+
 	render: function() {
 		var {action, addMeal} = this.props
 		if (action === 'none') return
 		return (
-			<p className='pull-right'>
-        {action === 'buy'
-        	&&
-	        	<a 
-			        className='btn btn-primary btn-outline' 
-			        role='button' 
-			        onClick={addMeal}>
-			        Buy
-			      </a>
-        }
-      </p>
+			<div>
+				<p className='pull-right'>
+	        {action === 'buy'
+	        	&&
+		        	<a 
+				        className='btn btn-primary btn-outline' 
+				        role='button' 
+				        onClick={this.openDialog}>
+				        Buy
+				      </a>
+	        }
+	      </p>
+	      <Modal
+	  			show={this.state.open}
+	  			onHide={this.closeDialog}
+	  		>
+	  			<Modal.Header closeButton>
+	  				<Modal.Title>Almost yours!</Modal.Title>
+	  			</Modal.Header>
+	  			<Modal.Body>
+	          <p>Once you buy this meal.. blah blah blah</p>
+	        </Modal.Body>
+	        <Modal.Footer>
+	          <button className='btn btn-primary btn-outline' onClick={() => {
+		          	addMeal() 
+		          	this.closeDialog()
+		          }}
+	          >
+	            Buy
+	          </button>
+	        </Modal.Footer>
+	  		</Modal>
+	  	</div>
 		)
 	}
 
