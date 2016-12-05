d161fe76c3f5e44a824611eddf1a4793943c234f
Nicola Pedretti <np.pedretti@gmail.com>
2016-12-05 10:46:58 -0600

    fixing indent spacing test 2

--- app/tools/pricePeople.js
+++ app/tools/pricePeople.js
@@ -9,12 +9,12 @@ module.exports = {
   /*
   * calculates the current price of an item
   * @param people {array} people cutoffs. e.g. [5, 10, 15] means
-  *	  price changes at 5 people, 10 people and 15 people
+  *   price changes at 5 people, 10 people and 15 people
   * @param prices {array} prices cutoffs. e.g [8.25, 7, 6, 5] price is 8.25
-  *	  if people less than people[0], 7 if people between people[0] and
-  *	  people[1], 6 if people between people[1] and peoplel[2] etc.
-  *	@param nOrders {number} current number of orders for a meal
-  *	@return {number} current price of the meal
+  *   if people less than people[0], 7 if people between people[0] and
+  *   people[1], 6 if people between people[1] and peoplel[2] etc.
+  * @param nOrders {number} current number of orders for a meal
+  * @return {number} current price of the meal
   */
   currentPrice: function (people, prices, nOrders) {
     for (var i = 0; i < _.size(people); i++) {
@@ -23,11 +23,11 @@ module.exports = {
     return _.min(prices)
   },
   /*
-	* calculates default price
-	* really simple now. I created a separate method for this in case we need to
-	* change logic later we only need to change it here
-	* @param prices {array} prices cutoff
-	*	@return {number} default price
+  * calculates default price
+  * really simple now. I created a separate method for this in case we need to
+  * change logic later we only need to change it here
+  * @param prices {array} prices cutoff
+  *  @return {number} default price
   */
   defaultPrice: function (prices) {
     return prices[0]
