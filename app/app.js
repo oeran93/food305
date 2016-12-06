9c280a7024e65367cbff4a3f003f238541c171d4
Nicola Pedretti <np.pedretti@gmail.com>
2016-12-05 20:55:21 -0600

    fixing indent

--- app/app.js
+++ app/app.js
@@ -12,24 +12,24 @@ var search = require('./backend/search/router.js')
 var auth = require('./backend/auth/router.js')
 
 module.exports = function (db) {
-	// Start DB
-	db() 
-	// Set up express
-	app.use(bodyParser.json())
-	app.use(bodyParser.urlencoded({extended: true}))
-	app.use(session({
-	  secret: env.SESSION_SECRET,
-	  name: 'session',
-	  resave: true,
-	  saveUninitialized: true
-	}))
-	app.use(passport.initialize())
-	app.use(passport.session())
-	// Do basic routing
-	app.use(express.static(path.join(__dirname, './frontend/static')))
-	search(app)
-	auth(app, passport)
-	return app;
+  // Start DB
+  db() 
+  // Set up express
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(session({
+    secret: env.SESSION_SECRET,
+    name: 'session',
+    resave: true,
+    saveUninitialized: true
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  // Do basic routing
+  app.use(express.static(path.join(__dirname, './frontend/static')))
+  search(app)
+  auth(app, passport)
+  return app;
 }
 
 
