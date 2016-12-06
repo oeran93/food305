d02b10ece7bc1c06b404e909aec60cd2ca536ad7
Nicola Pedretti <np.pedretti@gmail.com>
2016-12-05 20:51:21 -0600

    fixing indent

--- app/frontend/views/shareLinks.jsx
+++ app/frontend/views/shareLinks.jsx
@@ -2,17 +2,17 @@ var React = require('react')
 
 var ShareLinks = React.createClass({
 
-	componentDidMount: function () {
-		if (window.FB) {
-			window.FB.XFBML.parse();
-		}
-	},
+  componentDidMount: function () {
+    if (window.FB) {
+      window.FB.XFBML.parse();
+    }
+  },
 
-	render: function() {
-		return (
-			<div className="fb-send" data-href="http://nicolapedretti.com"></div>
-		)
-	}
+  render: function() {
+    return (
+     <div className="fb-send" data-href="http://nicolapedretti.com"></div>
+    )
+  }
 
 })
 
