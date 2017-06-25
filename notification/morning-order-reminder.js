1fc69e287c0c37444adcf9d20e7c2d9766c98b76
Nicola Pedretti <np.pedretti@gmail.com>
2017-06-25 14:45:11 -0500

    fixing message reminder

--- notification/morning-order-reminder.js
+++ notification/morning-order-reminder.js
@@ -4,7 +4,7 @@ const twilio = require('../tools/twilio.js')()
 
 User.find({}, 'phone', (err, users) => {
   users.forEach(u => {
-    twilio.send_sms(u.phone, "Hungry yet? remember to order your lunch on Vimi.
+    twilio.send_sms(u.phone, "Hungry yet? remember to order your lunch on Vimi.\
       Today's order closes at 11 am. Your order will be delivered at noon"
     )
   })
