e0b1c56a8b5b10b5bd84c1727ead9cd7709ac185
Nicola Pedretti <np.pedretti@gmail.com>
2017-07-06 09:39:49 -0500

    fixing grammar mistake

--- notification_center/successful_order.js
+++ notification_center/successful_order.js
@@ -1,6 +1,6 @@
 module.exports = function (infos) {
   return {
-    subject: `Your ${infos.meal_name} is on it's way!`,
+    subject: `Your ${infos.meal_name} is on its way!`,
     body: `${infos.name}, your ${infos.meal_name} is on its way. It will be delivered on ${infos.day} at ${infos.hour}`
   }
 }
