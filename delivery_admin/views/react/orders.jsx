202c5c731e7e6831f2b42da77fb6b7c9367762c8
Guanyu Chen <decrecyc@gmail.com>
2017-07-12 14:43:12 -0500

    before experiment with datepicker

--- delivery_admin/views/react/orders.jsx
+++ delivery_admin/views/react/orders.jsx
@@ -19,14 +19,6 @@ class Orders extends React.Component {
     })
   }
 
-<<<<<<< HEAD
-  count_orders (orders){
-    let count = {}
-    for(let i in orders){
-      let meal = orders[i].meal
-      count[meal] = count[meal] ? count[meal]+1 : 1
-    }
-=======
   count_orders (orders) {
     let count = {}
     _.each(orders, (order) => {
@@ -35,13 +27,11 @@ class Orders extends React.Component {
         ? count[meal] + 1
         : 1
     })
->>>>>>> c01414d617aa6915d648538d507ca97153a9cb1f
     return count
   }
 
   render () {
     let {orders} = this.state
-    console.log(orders)
     let count = this.count_orders(orders)
     return (
       <div>
@@ -54,37 +44,21 @@ class Orders extends React.Component {
             </tr>
           </thead>
           <tbody>
-<<<<<<< HEAD
-          {
-            Object.keys(count).map((meal,i) => {
-              return (
-                <tr key={i}>
-=======
             {
              _.map(count, (amount, meal) => {
               return (
                 <tr key={meal}>
->>>>>>> c01414d617aa6915d648538d507ca97153a9cb1f
                   <td>
                     <p>{meal}</p>
                   </td>
                   <td>
-<<<<<<< HEAD
-                    <p>{count[meal]}</p>
-=======
                     <p>{amount}</p>
->>>>>>> c01414d617aa6915d648538d507ca97153a9cb1f
                   </td>
                 </tr>
               )
             })
-<<<<<<< HEAD
-          }
-         </tbody>
-=======
            }
           </tbody>
->>>>>>> c01414d617aa6915d648538d507ca97153a9cb1f
         </table>
         <h2>Orders per User</h2>
         <table className="table table-bordered">
