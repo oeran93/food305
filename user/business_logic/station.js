11b9af82f78092115820d6d2da2b6ba793522f7d
Nicola Pedretti <np.pedretti@gmail.com>
2017-09-17 12:41:36 -0500

    filtering stations by hidden

--- user/business_logic/station.js
+++ user/business_logic/station.js
@@ -9,7 +9,7 @@ module.exports = function () {
   * Returns station info
   */
   pub.info = function (req, res) {
-    Station.findOne({_id: req.session.user.station}, (err, station) => {
+    Station.findOne({_id: req.session.user.station, hidden: false}, (err, station) => {
       res.send(station)
     })
   }
@@ -18,7 +18,7 @@ module.exports = function () {
   * Returns all available stations
   */
   pub.get_stations = function (req, res) {
-    Station.find({}, (err, stations) => {
+    Station.find({hidden: false}, (err, stations) => {
       res.send(stations)
     })
   }
