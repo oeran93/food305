8f077e190d9d9bb9a4caaab7aeaae1e17965a607
Nicola Pedretti <np.pedretti@gmail.com>
2017-09-18 22:57:14 -0500

    eliminating comments

--- curator/business_logic/get_restaurants.js
+++ curator/business_logic/get_restaurants.js
@@ -31,9 +31,7 @@ module.exports = function () {
               })
             })
             const avg_rating = isNaN(sum/count) ? "" : sum/count
-            // console.log(JSON.stringify(restaurants))
             restaurants[index] = restaurants[index].toObject()
-            // console.log(JSON.stringify(restaurants))
             restaurants[index]["rating"] = isNaN(sum/count) ? "" : avg_rating.toFixed(2)
           })
 
