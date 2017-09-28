3ea5508363b2a92dde505dc9be8f8b536a6f728e
Nicola Pedretti <np.pedretti@gmail.com>
2017-09-27 22:38:46 -0500

    modifying text message

--- notification_center/successful_order.js
+++ notification_center/successful_order.js
@@ -1,6 +1,6 @@
 module.exports = function (infos) {
   return {
     subject: `Your ${infos.meal_name} is on its way!`,
-    body: `${infos.name}, your ${infos.meal_name} is on its way. It will be delivered on ${infos.day} at ${infos.hour}`
+    body: `${infos.name}, your ${infos.meal_name} is on its way. It will be delivered ${infos.day} at ${infos.hour}`
   }
 }
