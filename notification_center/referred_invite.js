074c9ad5e1f8f411fc72aaa90452003dfd67d8f6
Nicola pedretti <np.pedretti@gmail.com>
2017-10-19 23:54:50 -0500

    fix amount discount in emails

--- notification_center/referred_invite.js
+++ notification_center/referred_invite.js
@@ -2,7 +2,7 @@ module.exports = function (infos) {
   return {
     subject: "Vimi, a friend invited you to join!",
     body: `Hi, your friend ${infos.name} invited you to join Vimi.
-    When you sign up both of you get 50% off the purchase of your first meal.
+    When you sign up both of you get 25% off the purchase of your first meal.
     <br/><br/><a href="https://vimifood.com">Find out more about us and sign up<a/>
     `
   }
